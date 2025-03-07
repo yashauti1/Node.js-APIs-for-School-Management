83acdb2f2a9ce3b978e6b432a11ab715c616dfa1
Yash Auti <yashauti2471@gmail.com>
2025-03-08 00:46:00 +0530

    modify db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,26 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const db = await mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+// console.log("✅ MySQL Database Connected!");
+
+// export default db;
+
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+console.log("connection sucessfull");
 
-console.log("✅ MySQL Database Connected!");
-
-export default db;
+export default connection;
