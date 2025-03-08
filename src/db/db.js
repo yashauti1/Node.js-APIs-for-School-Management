bb2b7e9c4616ea65a26f5344211581a54fba3bcc
Yash Auti <yashauti2471@gmail.com>
2025-03-08 11:06:07 +0530

    modify db

--- src/db/db.js
+++ src/db/db.js
@@ -15,12 +15,30 @@
 
 // export default db;
 
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL);
-console.log("connection sucessfull");
+// const connection = await mysql.createConnection(process.env.MYSQL_URl);
+// console.log("connection sucessfull");
 
-export default connection;
+// export default connection;
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST, 
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+console.log("✅ MySQL Database Connected!");
+export default pool;
+
