2defb5629972b75c98099e5b1e33ec6b0f813923
Yash Auti <yashauti2471@gmail.com>
2025-03-08 12:10:05 +0530

    modify deploy

--- src/db/db.js
+++ src/db/db.js
@@ -30,11 +30,11 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST, 
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT || 3306, 
+  host: process.env.MYSQLHOST, 
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
